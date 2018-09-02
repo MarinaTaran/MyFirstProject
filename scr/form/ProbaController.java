a53f893dc00bde0556de8b182628d0f9a471be92
Marina <marinataran27@gmail.com>
2018-09-02 12:49:05 +0300

    second commit

--- scr/form/ProbaController.java
+++ scr/form/ProbaController.java
@@ -1,28 +1,63 @@
 package form;
 
+import entity.User;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ProbaController {
     @FXML
-    Button btn1;
+    Button reg;
     @FXML
 Button login;
     @FXML
 TextField text_emeil;
     @FXML
 PasswordField password;
+    @FXML
+    Label message;
 
-    public  void one(){
-        System.out.println("sdsddssdsds");
+
+    public  void startReg(){
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("registration.fxml"));
+       // System.out.println(getClass().getResource("two.fxml"));
+        Parent root2 = null;
+        try {
+            root2 = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(root2));
+        stage.show();
+
     }
 
-    public void login(){
-       System.out.println(text_emeil.getText());
-//        System.out.println(password.getText());
-        System.out.println("sdsdsdsds");
-    }
+    public boolean login(){
+        boolean result=false;
+       String email=text_emeil.getText();
+        String pas=password.getText();
+        System.out.println(email+"qqqqq");
+        System.out.println(pas+"wwwwww");
+        if(User.login(email,pas)==null){
+            System.out.println("Ошибка авторизации !");
+        }else{
+            result=true;
+            message.setText("Congratulation !!!");
+            message.setVisible(true);
+        }
+        return  result;
+          }
 
 }
