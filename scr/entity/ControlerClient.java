2f88354b70e9a8b772689c7ee9d0376af06e2eec
Marina <marinataran27@gmail.com>
2018-09-02 12:47:10 +0300

    second commit

---
+++ scr/entity/ControlerClient.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class ControlerClient {  //otvechaet za logirovanie,za bizness logiku GRASP
+   private  DaoUser daoUser=new DaoClient();
+
+    boolean isThereUser(String name,String email){
+       boolean result=false;
+       return result;
+    }
+
+
+}
