8c98cb157a5bf08ceb76ff0384cc81be7d7504b3
Marina <marinataran27@gmail.com>
2018-09-02 14:24:04 +0300

    second commit

--- scr/Start1.java
+++ scr/Start1.java
@@ -1,3 +1,5 @@
+import entity.Client;
+import entity.User;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,11 +9,17 @@ import javafx.stage.Stage;
 public class Start1 extends Application
 
 {
+    static{
+        new Client("mari","123","mari@gmail.com","222333");
+        new Client("mari1","123","mari1@gmail.com","222333");
+        new Client("mari2","123","mari2@gmail.com","222333");
+
+    }
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("form/proba.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
 
