26184e4b34ab0b9d550e65bac67294ab5c04549e
Marina <marinataran27@gmail.com>
2018-09-12 09:12:32 +0300

    commit

--- scr/form/ProbaController.java
+++ scr/form/ProbaController.java
@@ -18,19 +18,19 @@ public class ProbaController {
     @FXML
     Button reg;
     @FXML
-Button login;
+    Button login;
     @FXML
-TextField text_emeil;
+    TextField text_emeil;
     @FXML
-PasswordField password;
+    PasswordField password;
     @FXML
     Label message;
 
 
-    public  void startReg(){
+    public void startReg() {
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("registration.fxml"));
-       // System.out.println(getClass().getResource("two.fxml"));
+        // System.out.println(getClass().getResource("two.fxml"));
         Parent root2 = null;
         try {
             root2 = (Parent) fxmlLoader.load();
@@ -44,20 +44,39 @@ PasswordField password;
 
     }
 
-    public boolean login(){
-        boolean result=false;
-       String email=text_emeil.getText();
-        String pas=password.getText();
-        System.out.println(email+"qqqqq");
-        System.out.println(pas+"wwwwww");
-        if(User.login(email,pas)==null){
-            System.out.println("Ошибка авторизации !");
-        }else{
-            result=true;
-            message.setText("Congratulation !!!");
+    public boolean login1() {
+        boolean result = false;
+        String email = text_emeil.getText();
+        String pas = password.getText();
+        System.out.println(email + "email");
+        System.out.println(pas + "password");
+        if (User.login(email, pas) == null) {
+            System.out.println("Authorisation Error !");
+        } else {
+            result = true;
+            message.setText("Congratulation !!! You authorized");
             message.setVisible(true);
         }
-        return  result;
-          }
+        return result;
+    }
 
+
+
+    public  void goToOrder(){
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("orderForUser.fxml"));
+        // System.out.println(getClass().getResource("two.fxml"));
+        Parent root2 = null;
+        try {
+            root2 = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(root2));
+        stage.show();
+
+    }
 }
+
