862836881d17202c54ae7b50d2a7f73bbc4cf4fa
Marina <marinataran27@gmail.com>
2018-09-12 09:01:59 +0300

    method updateUser

--- scr/entity/DaoClient.java
+++ scr/entity/DaoClient.java
@@ -1,19 +1,17 @@
 package entity;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class DaoClient implements DaoUser {
 
     @Override
-    public User createUser(String name, String emeil, String password, String telephone) throws ClientExceptin {
+    public User createUser(String name, String emeil, String password, String telephone) throws ClientException {
         User result = null;
         if (findUserforEmeil(emeil) == null) {
             result = new Client(name, password, emeil, telephone);
             result.getListUsers().add(result);
         } else {
-            throw new ClientExceptin("Дублирование клиента !");
+            throw new ClientException("Such a customer already exists  !");
         }
 
         return result;
@@ -22,47 +20,55 @@ public class DaoClient implements DaoUser {
     @Override
     public boolean remove(User user) {
         for (User temp : findeAllUser()) {
-            if (temp.email.equals(user)) {
-
+            if (temp.getEmail().equals(user)) {
+//            temp.email.
             }
         }
-
         return false;
     }
 
     @Override
-    public User update(User user) {
-
-
-        return null;
+    public User update(String emeil, User temp) {
+       User resulrt=null;
+        User oldUser=findUserforEmeil(emeil);
+        if(oldUser==null){
+            System.out.println("net takogo usere");
+        }else{
+            oldUser.password=temp.password;
+            oldUser.name=temp.name;
+            oldUser.telephon=temp.telephon;
+        }
+        return resulrt;
     }
 
 
     @Override
-    public User findUserByID(int ID) {
-        User result = null;
-        for (User temp : findeAllUser()) {
-            if (temp.ID == ID) {
-                result = temp;
+        public User findUserByID ( int ID){
+            User result = null;
+            for (User temp : findeAllUser()) {
+                if (temp.ID == ID) {
+                    result = temp;
+                }
+
+            }
+            return result;
+        }
+
+        @Override
+        public List<User> findeAllUser () {
+
+            return User.getListUsers();
+        }
+
+        @Override
+        public User findUserforEmeil (String emeil){
+            User result = null;
+            for (User temp : findeAllUser()) {
+                if (temp.getEmail().equals(emeil)) {
+                    result = temp;
+                }
             }
 
+            return result;
         }
-        return result;
     }
-
-    @Override
-    public List<User> findeAllUser() {
-
-        return User.getListUsers();
-    }
-
-    @Override
-    public User findUserforEmeil(String emeil) {
-        if (findeAllUser().equals(emeil)) {
-
-        }
-
-
-        return null;
-    }
-}
